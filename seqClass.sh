48e105459a1745f04654cd298e97675c028fe5a2
Roc Farriol Duran <rocfarriolduran@Rocs-MacBook-Pro.local>
2020-04-13 14:54:53 +0200

    motif

--- seqClass.sh
+++ seqClass.sh
@@ -12,3 +12,12 @@ if [[ $seq =~ ^[ACGTU]+$ ]]; then
 else
   echo "The sequence is either DNA nor RNA"
 fi
+motif=$(echo $2 | tr a-z A-Z)
+if [[ -n $motif ]]; then
+  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
+  if [[ $seq =~ $motif ]]; then
+    echo "FOUND"
+  else
+    echo "NOT FOUND"
+  fi
+fi
