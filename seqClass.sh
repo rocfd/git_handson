43d97c08ddd1a464ecce82d301ef46d6ece2927f
rocfd <rocfd95@gmail.com>
2020-04-15 18:13:02 +0200

    Code comments

--- seqClass.sh
+++ seqClass.sh
@@ -3,10 +3,10 @@
 seq=$1
 seq=$(echo $seq | tr a-z A-Z)  # Line to capitalize lower-case inputs.
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
-  if [[ $seq =~ T ]]; then
+  if [[ $seq =~ T ]]; then     # Uses T as hallmark for DNA sequences.
     echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
+  elif [[ $seq =~ U ]]; then   # Uses U as hallmark for RNA sequences.
     echo "The sequence is RNA"
-  else
+  else			       # When no T nor U is present it cannot distinguish.
     echo "The sequence is either DNA nor RNA"
 fi
